116ff71408a299394de5f47515fd4876c832a705
paragshelke <paragshelke701@gmail.com>
2025-07-06 16:31:13 +0530

    Add files via upload

---
+++ C programming/Tests/elcetricity bill.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+	int u;
+	int bill;
+	
+	printf("enter units");
+	scanf("%d",&u);
+	
+	if(u<=50)
+	{
+		bill=u*30;
+	}
+	if(u>=51&&u<=150)
+	{
+		bill=u*40;
+	}
+	else
+	{
+		bill=u*50;
+	}
+	printf("electricity bill is %d",bill);
+}
