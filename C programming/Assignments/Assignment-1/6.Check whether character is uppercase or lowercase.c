5fcc8c99ad8832caaa1756857bfaf461ae556633
paragshelke <paragshelke701@gmail.com>
2025-07-06 23:13:04 +0530

    Add files via upload

---
+++ C programming/Assignments/Assignment-1/6.Check whether character is uppercase or lowercase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	char c;
+	
+	printf("enter a character ");
+	scanf("%c",&c);
+	
+	if(c>='A'&&c<='Z')
+	{
+		printf("character is Uppercase ");
+	}
+	else
+	{
+		printf("character is Lowercase");
+	}
+}
