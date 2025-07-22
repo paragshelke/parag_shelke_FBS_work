56b862784fe6b6a3176b98104418ed92a0c9fe03
paragshelke <paragshelke701@gmail.com>
2025-07-23 01:11:23 +0530

    Add files via upload

---
+++ C programming/Assignments/Assignment-008/001.Find minimum and maximum number in array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void main()
+{
+	int n;
+	printf("Enter size of your array : ");
+	scanf("%d",&n);
+	
+	int arr[n];
+	printf("Enter %d numbers : ");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int max=arr[0];
+	int min=arr[0];
+	for(int i=0;i<n;i++)
+	{
+		if(max<arr[i])
+		{
+			max=arr[i];
+		}
+		if(min>arr[i])
+		{
+			min=arr[i];
+		}
+	}
+	printf("maximum number is %d",max);
+	printf("\nminium number is %d",min);
+}
