06492fb4263a4cb92ef7b6f304517fbc48641085
paragshelke <paragshelke701@gmail.com>
2025-07-31 17:52:42 +0530

    Add files via upload

---
+++ C programming/Tests/Test-8/001.verify id and password.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	int id;
+	int password;
+	int a=1234,b;
+	
+	printf("enter id and password : ");
+	scanf("%d %d",&id,&password);
+	
+	printf("enter the following number %d : ",a);
+	scanf("%d",&b);
+	
+	if(b==1234)
+		printf("sucess");
+	else
+		printf("fail");
+		
+}
